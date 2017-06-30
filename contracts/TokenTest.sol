0bdd75332a3fb8482a6816b71ec62dae044135d4
SylTi <sylvain.tissier@gmail.com>
2017-06-30 13:31:24 +0200

    add zeppelin lib

--- contracts/TokenTest.sol
+++ contracts/TokenTest.sol
@@ -3,8 +3,11 @@
 pragma solidity ^0.4.10;
 
 import "./libs/HumanStandardToken.sol";
+// import "zeppelin-solidity/contracts/Ownable.sol";
+import "./libs/zeppelin/ownership/Ownable.sol";
+
 // Parent paramaters : tokenAmount, tokeName, decimals, symbol
-contract TokenTest is HumanStandardToken(1000000, "Sample Token", 18, "STERC20") {
+contract TokenTest is HumanStandardToken(1000000, "Sample Token", 18, "STERC20"), Ownable {
 
     bool public _circuitBreaker;
     address public _owner;
@@ -14,22 +17,22 @@ contract TokenTest is HumanStandardToken(1000000, "Sample Token", 18, "STERC20")
         _owner = msg.sender;
     }
 
-    modifier isOwner() {
-        require(_owner == msg.sender);
-        _;
-    }
-
     modifier circuitBreaker() {
         require(_circuitBreaker == false);
         _;
     }
 
-    function setCircuitBreaker(bool stop) isOwner {
+    modifier onlyPayloadSize(uint size) {
+        assert(msg.data.length >= size + 4);
+        _;
+    }
+
+    function setCircuitBreaker(bool stop) onlyOwner {
         _circuitBreaker = stop;
     }
 
     //overload parent function with circuitBreaker
-    function transfer(address _to, uint256 _value) circuitBreaker returns (bool success)  {
+    function transfer(address _to, uint256 _value) circuitBreaker onlyPayloadSize(2 * 32) returns (bool success)  {
         return super.transfer(_to, _value);
     }
 }
