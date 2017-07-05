527cd04f2a0b1b401bbeb0fdaeed09489e4f7cdc
SylTi <sylvain.tissier@gmail.com>
2017-07-05 18:36:41 +0200

    refactoring

--- contracts/TokenTest.sol
+++ contracts/TokenTest.sol
@@ -4,33 +4,29 @@ pragma solidity ^0.4.10;
 
 import "./libs/HumanStandardToken.sol";
 // import "zeppelin-solidity/contracts/Ownable.sol";
-import "./libs/zeppelin/ownership/Ownable.sol";
+// import "./libs/zeppelin/ownership/Ownable.sol";
+import "./libs/zeppelin/lifecycle/Pausable.sol";
+// import "./libs/zeppelin/token/SimpleToken.sol";
 
 // Parent paramaters : tokenAmount, tokeName, decimals, symbol
-contract TokenTest is HumanStandardToken(1000000, "Sample Token", 18, "STERC20"), Ownable {
+contract TokenTest is HumanStandardToken(1000000, "Sample Token", 18, "STERC20"), Pausable {
 
-    bool public _circuitBreaker;
+    // For usage with SimpleToken instead of HumanStandardToken. Could also use PausableToken instead of Pausable
+    // string public name = "Sample Token";
+    // string public symbol = "STERC20";
+    // uint public decimals = 18;
+    // uint public INITIAL_SUPPLY = 1000000;
 
     function TokenTest() {
-        _circuitBreaker = false;
     }
 
-    modifier circuitBreaker() {
-        require(_circuitBreaker == false);
-        _;
-    }
-
-    modifier onlyPayloadSize(uint size) {
-        assert(msg.data.length >= size + 4);
-        _;
-    }
-
-    function setCircuitBreaker(bool stop) onlyOwner {
-        _circuitBreaker = stop;
-    }
+    // modifier onlyPayloadSize(uint size) onlyPayloadSize(2 * 32) {
+    //     assert(msg.data.length >= size + 4);
+    //     _;
+    // }
 
     //overload parent function with circuitBreaker
-    function transfer(address _to, uint256 _value) circuitBreaker onlyPayloadSize(2 * 32) returns (bool success)  {
+    function transfer(address _to, uint256 _value) whenNotPaused returns (bool success)  {
         return super.transfer(_to, _value);
     }
 }
